57475981a71ab094148064b4090a9015852be857
Ismail <114358862+Ismail970@users.noreply.github.com>
2023-01-17 09:23:36 -0800

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => menu.classList.toggle("hidden"));
